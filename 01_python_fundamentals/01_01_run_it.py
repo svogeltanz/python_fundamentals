2f848d2e54ade05ac5b469e876d4b52899af66ab
Sebastian Vogeltanz <sebvogeltanz@gmail.com>
2019-07-25 20:24:06 +0200

    worked on the first three sections

--- 01_python_fundamentals/01_01_run_it.py
+++ 01_python_fundamentals/01_01_run_it.py
@@ -1,5 +1,5 @@
 '''
-1 - Write and execute a script that prints "hello world" to the console.
+1 - Write and execute a script that prints "hello world!" to the console.
 
 2 - Using the interpreter, print "hello world!" to the console.
 
@@ -14,3 +14,29 @@
 	- Calculate how many seconds are in a year.
 
 '''
+
+print("Hello world!")
+
+name = input("Please enter your name: ")
+
+print("And hello " + name + " as well. We do have a little exercise for you today!")
+
+a = int(input("Please enter a number: "))
+b = int(input("Please enter a second number: "))
+c = int(input("Please enter a third number: "))
+
+result = a + b + c
+
+print("The sum of your numbers is: " + str(result) + ".")
+
+
+# calculation how many seconds are in one year
+
+days = 365
+hours = 24
+minutes = 60
+seconds = 60
+
+secondsPerYear = seconds * hours * minutes * days
+
+print("In one year are: " + str(secondsPerYear) + " seconds.")
--- 01_python_fundamentals/01_02_seconds_years.py
+++ 01_python_fundamentals/01_02_seconds_years.py
@@ -3,3 +3,15 @@
 From the previous example, move your calculation of how many seconds in a year to a python executable script.
 
 '''
+
+print("In this Script is calculated how many seconds are in one year. Therefore you have to enter the correct numbers "
+      "for the day, hours, minutes and seconds")
+
+days = int(input("Please enter the amount of days in one year: "))
+hours = int(input("Please enter the amount of hours in one day: "))
+minutes = int(input("Please enter the amount of minutes in one hour: "))
+seconds = int(input("Please enter the amount of seconds in one minute: "))
+
+secondsPerYear = seconds * hours * minutes * days
+
+print("In one year are: " + str(secondsPerYear) + " seconds.")
