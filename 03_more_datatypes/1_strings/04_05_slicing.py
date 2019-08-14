22d27a49ca7a8ffffd026dbdece55364cb67a7b5
Sebastian Vogeltanz <sebvogeltanz@gmail.com>
2019-08-14 19:12:58 +0200

    finished section with strings

--- 03_more_datatypes/1_strings/04_05_slicing.py
+++ 03_more_datatypes/1_strings/04_05_slicing.py
@@ -8,3 +8,15 @@ For example: ryan -> yanray, caden -> adencay
 
 '''
 
+# take in the user's name
+user_name = input("Please enter your name: ")
+print(user_name)
+
+# slicing the first char to a new variable and move it to the end. Add "ay" to the string finally
+add_pig_latin = "ay"
+user_name_first = user_name[0]
+user_name_new = user_name[1:]
+user_name_translated = user_name_new + user_name_first + add_pig_latin
+
+# print the translated name
+print(user_name_translated)
