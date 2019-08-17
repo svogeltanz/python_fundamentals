12a8f3039c6830bd5fe37c0fdf939ab40d8b89ac
Sebastian Vogeltanz <sebvogeltanz@gmail.com>
2019-08-17 17:45:45 +0200

    worked on the first exercise

--- 03_more_datatypes/2_lists/04_06_product_largest.py
+++ 03_more_datatypes/2_lists/04_06_product_largest.py
@@ -9,3 +9,27 @@ that we haven't looked at yet. See if you can figure it out, otherwise
 come back to this task after you have learned about loops)
 
 '''
+
+# while the length of the list is < 11
+user_input_list = []
+# take in a number and append it to the list
+while len(user_input_list) < 10:
+    user_input = int(input("Please enter a number for your list: "))
+    user_input_list.append(user_input)
+
+# print the list
+print(user_input_list)
+
+# find the largest number using max()
+max_number = max(user_input_list)
+
+# print the largest number
+print("The max value of your list is: " + str(max_number))
+
+# calculate the product of all number of the list using a for loop
+result = 1
+for number in user_input_list:
+    result = result * number
+
+print("The product of the whole list is: ")
+print(result)
