eef5fe8599485034ad195275c44fb324c8afff82
Sebastian Vogeltanz <sebvogeltanz@gmail.com>
2019-08-23 11:31:34 +0200

    finished the dictionary labs

--- 03_more_datatypes/4_dictionaries/04_18_common.py
+++ 03_more_datatypes/4_dictionaries/04_18_common.py
@@ -8,3 +8,19 @@ dict_2 = {"a": 2, "c": 4 , "d": 2}
 result = {"a": 3, "b": 2, "c": 7 , "d": 2}
 
 '''
+
+dict_1 = {"a": 1, "b": 2, "c": 3}
+dict_2 = {"a": 2, "c": 4 , "d": 2}
+
+# create a function which adds the values for the same key
+def dictAdd(dict1, dict2):
+    dict_result = {**dict_1, **dict_2}
+    for key, value in dict_result.items():
+        if key in dict_1 and key in dict_2:
+            dict_result[key] = value + dict_1[key]
+
+    return dict_result
+
+# merge two dictionaries
+result_dict = dictAdd(dict_1, dict_2)
+print(result_dict)
--- 03_more_datatypes/4_dictionaries/04_19_dict_tuples.py
+++ 03_more_datatypes/4_dictionaries/04_19_dict_tuples.py
@@ -5,3 +5,10 @@ input_dict = {"item1": 5, "item2": 6, "item3": 1}
 result_list = [("item3", 1), ("item1", 5), ("item2", 6)]
 
 '''
+
+input_dict = {"item1": 5, "item2": 6, "item3": 1}
+
+sorted_dict = sorted(input_dict.items() , key = lambda x : x[1])
+# Question for Johnny: What's going on with lambdas??
+
+print(sorted_dict)
