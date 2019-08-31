347e750673c8e8435708a3e38cb7f1e94849bdfc
Sebastian Vogeltanz <sebvogeltanz@gmail.com>
2019-08-31 12:59:54 +0200

    Solved the first function

--- 05_functions/05_01_tasks.py
+++ 05_functions/05_01_tasks.py
@@ -13,3 +13,20 @@ Write a script that completes the following tasks.
 # call your functions, passing in the user input as the arguments, and set their output equal to new variables 
 
 # print your new variables to display the results
+
+def is_divisible_4or7(number):
+    if number % 4 == 0 or number % 7 == 0:
+        return True
+    return False
+
+def is_divisible_4and7(number):
+    if number % 4 == 0 and number % 7 == 0:
+        return True
+    return False
+
+user_input = int(input("Please enter a number between 1 and 1,000,000,000: "))
+
+fourORseven = is_divisible_4or7(user_input)
+fourANDseven = is_divisible_4and7(user_input)
+
+print(f"Your number is divisible by 4 or 7: {fourORseven}\nYour number is divisible by 4 and 7: {fourANDseven}")
