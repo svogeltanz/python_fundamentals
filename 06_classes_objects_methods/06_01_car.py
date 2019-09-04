5cd5e3cbec0cd6c490d104ef3aa40b16f3575f5a
Sebastian Vogeltanz <sebvogeltanz@gmail.com>
2019-09-04 19:52:20 +0200

    created the first two classes/finished two exercises

--- 06_classes_objects_methods/06_01_car.py
+++ 06_classes_objects_methods/06_01_car.py
@@ -9,3 +9,27 @@ Create at least two different objects of this Car class and demonstrate
 changing the objects attributes.
 
 '''
+
+class Car:
+
+    def __init__(self, model, year, max_speed):
+        self.model = model
+        self.year = year
+        self.max_speed = max_speed
+
+    def speed_increase(self):
+        self.max_speed += 5
+
+    def __str__(self):
+        return f"Model: {self.model}, Year: {self.year}, Speed: {self.max_speed}"
+
+speedy = Car("Lambo", 1994, 330)
+
+james_bond = Car("Aston Martin", 1965, 230)
+
+speedy.speed_increase()
+
+speedy.speed_increase()
+
+print(speedy)
+
--- 06_classes_objects_methods/06_02_shapes.py
+++ 06_classes_objects_methods/06_02_shapes.py
@@ -7,3 +7,57 @@ Write methods in the appropriate class so that you can calculate the area (of th
 perimeter (of the rectangle) and circumference of the circle.
 
 '''
+
+
+# create class for rectangle; constructed by length and width
+# write method to calculate area of both classes
+# write method to calculate perimeter
+class Rectangle:
+    '''Class for a rectangle with two methods'''
+
+    def __init__(self, length, width):
+        '''Constructor which takes a length and width'''
+        self.length = length
+        self.width = width
+
+    def area(self):
+        '''Calculates the area of the rectangle'''
+        result = self.length * self.width
+        return result
+
+    def perimeter(self):
+        '''Calculates the perimeter of the rectangle'''
+        result = (self.length + self.width) * 2
+        return result
+
+
+# create class for circle; constructed by radius
+# write method to calculate circumference
+
+class Circle:
+    '''Class for a circle with two methods'''
+    # set pi "globally" here to use it in all methods
+
+    def __init__(self, radius):
+        '''Constructor which takes a radius'''
+        self.radius = radius
+
+    def area(self):
+        '''Calculates the area of the circle'''
+        pi = 3.14
+        result = pi * (self.radius**2)
+        return result
+
+    def circumference(self):
+        '''Calculates the circumference of the rectangle'''
+        pi = 3.14
+        result = pi * self.radius * 2
+        return result
+
+r = Rectangle(10, 5)
+print(r.area())
+print(r.perimeter())
+
+c = Circle(8)
+print(c.area())
+print(c.circumference())
