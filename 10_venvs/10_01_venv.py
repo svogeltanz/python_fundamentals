756be144ca392a859c8cc245d4edb2e0ec8c97fc
Sebastian Vogeltanz <sebvogeltanz@gmail.com>
2019-09-25 22:42:58 +0200

    Worked on (and finished) the venv labs

--- 10_venvs/10_01_venv.py
+++ 10_venvs/10_01_venv.py
@@ -5,9 +5,33 @@ In your CodingNomads folder create a new folder. Inside of that folder:
 2. Activate the virtual environment
 3. Install at least 3 packages in the virtual environment.
 4. Freeze the installed packages to a requirements.txt file.
+    how to output every installed module to a txt
 5. Deactivate the virtual environment.
 6. Delete the virtual environment.
 7. Create a new virtual environment and install the packages from the requirements.txt file.
 
+
+'''
+
+
+'''
+Phython Code to enter in the CLI
+
+1. python3 -m venv env
+
+2. source env/bin/activate
+
+3. pip install django
+   pip install requests
+   pip install pandas
+
+4. env/bin/pip freeze > requirements.txt
+
+5. deactivate
+
+6. rm -r env
+
+7.  python3 -m venv env1
+    env1/bin/pip install -r requirements.txt
 
 '''
