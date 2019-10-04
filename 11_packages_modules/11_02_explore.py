b29ebaeac5f283c44b8034df6de55449939eb0dd
Sebastian Vogeltanz <sebvogeltanz@gmail.com>
2019-10-04 09:30:18 +0200

    worked on the packages labs

--- 11_packages_modules/11_02_explore.py
+++ 11_packages_modules/11_02_explore.py
@@ -3,3 +3,19 @@ Do some research on other popular python packages and what the are used for. Fee
 and play around a little.
 
 '''
+
+# work on them!
+
+import requests
+import numpy
+import pandas as pd
+
+print("Requests:", requests.__version__)
+print("Numpy:", numpy.__version__)
+print("Pandas:", pd.__version__)
+
+pd.set_option('display.max_colums', 100)
+pd.set_option('display.max_rows', 5000)
+
+pd.read_excel("aging_example.xlsx")
+
