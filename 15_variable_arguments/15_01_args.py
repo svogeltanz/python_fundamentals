e3d2120d33ff2412e19542d5e8d1d348759a187b
Sebastian Vogeltanz <sebvogeltanz@gmail.com>
2019-10-07 19:13:17 +0200

    variable args done

--- 15_variable_arguments/15_01_args.py
+++ 15_variable_arguments/15_01_args.py
@@ -2,3 +2,10 @@
 Write a script with a function that demonstrates the use of *args.
 
 '''
+
+def my_function(*args):
+    for i in args:
+        print(i)
+
+
+my_function("hi", "how\'s", "it", "going", "?")
--- 15_variable_arguments/15_02_kwargs.py
+++ 15_variable_arguments/15_02_kwargs.py
@@ -2,3 +2,13 @@
 Write a script with a function that demonstrates the use of **kwargs.
 
 '''
+
+
+def pizza_rec(**kwargs):
+
+    for k, v in kwargs.items():
+        print(f"{k}: {v}")
+
+
+pizza_rec(first="make dough", second="select sauce", third="select topping", fourth="make pizza")
+
