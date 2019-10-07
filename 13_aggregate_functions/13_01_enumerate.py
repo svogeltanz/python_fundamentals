c805693ab57e11b1ed1662caa851dab3c67c08dd
Sebastian Vogeltanz <sebvogeltanz@gmail.com>
2019-10-07 13:37:12 +0200

    aggregate functions done

--- 13_aggregate_functions/13_01_enumerate.py
+++ 13_aggregate_functions/13_01_enumerate.py
@@ -1,3 +1,14 @@
 '''
 Demonstrate the use of the .enumerate() function.
 '''
+
+
+my_list = ["apple", "banana", "orange"]
+
+obj1 = list(enumerate(my_list))
+
+for index, item in obj1:
+    print(f"{index}: {item}")
+
+print(obj1)
+
--- 13_aggregate_functions/13_02_sum.py
+++ 13_aggregate_functions/13_02_sum.py
@@ -2,3 +2,18 @@
 Write a simple aggregate function, sum(), that takes a list a returns the sum.
 
 '''
+
+def sum(list):
+
+    '''Takes in a list and returns the sum'''
+
+    result = 0
+
+    for item in list:
+        result += item
+
+    return result
+
+list = [0, 3, 5, 7, 11, 16]
+
+print(sum(list))
