b99753775d9a7eff6c69416d1ec59b3d39ab6a16
Sebastian Vogeltanz <sebvogeltanz@gmail.com>
2019-10-07 11:01:29 +0200

    generators done

--- 17_generators/17_01_generators.py
+++ 17_generators/17_01_generators.py
@@ -3,3 +3,11 @@ Demonstrate how to create a generator object. Print the object to the console to
 Then iterate over the generator object and print out each item.
 
 '''
+
+gen = (x for x in range(5))
+
+print(gen)
+
+for num in gen:
+    print(num)
+
