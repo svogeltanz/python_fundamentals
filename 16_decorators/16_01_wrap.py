813ec7d803604173cd1ffd8080d9648acc05315f
Sebastian Vogeltanz <sebvogeltanz@gmail.com>
2019-10-09 16:40:09 +0200

    worked on decorators

--- 16_decorators/16_01_wrap.py
+++ 16_decorators/16_01_wrap.py
@@ -3,3 +3,33 @@ Write a decorator function that wraps text passed to it in a specified html tag.
 The user should be able to decide which tag to use.
 
 '''
+
+
+def decorator_func(initial_func):
+    def wrapper_func():
+        print("wrapper function picked some...")
+        return initial_func()
+    return wrapper_func  # returns the wrapper func ready to be executed
+
+
+# decorated_pretty = decorator_func(prettify)
+# decorated_pretty()  # executes wrapper function which executes prettify() func
+
+@decorator_func # is the same as the commented above
+def prettify():
+    print("flowers for you")
+
+
+@decorator_func
+def feed():
+    print("potato and vegan meat")
+
+
+@decorator_func
+def sports():
+    print("running shoes for you! See you later!")
+
+
+prettify()
+feed()
+sports()
