99a79bdc3dfadba732fcd5d1a6f51f31c082b6c4
Sebastian Vogeltanz <sebvogeltanz@gmail.com>
2019-10-12 13:41:24 +0200

    finished working on lambdas

--- 18_lambdas/18_01_first_lambda.py
+++ 18_lambdas/18_01_first_lambda.py
@@ -2,3 +2,10 @@
 Write a lambda function that takes in 3 numbers and returns the sum of the numbers.
 
 '''
+
+sum = lambda x, y, z: x + y + z
+
+example_function = sum(5, 7, 9)
+
+print(example_function)
+
--- 18_lambdas/18_02_return.py
+++ 18_lambdas/18_02_return.py
@@ -1,4 +1,11 @@
 '''
-Write a lambda function that does not take in an arguments but returns a value. Print the return value.
+Write a lambda function that does not take in an argument but returns a value. Print the return value.
 
 '''
+
+# lambda without intake arguments
+
+no_arg = lambda : print("No argument needed")
+
+no_arg()
+
