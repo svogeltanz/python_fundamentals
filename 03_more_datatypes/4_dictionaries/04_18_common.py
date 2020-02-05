1903d678bead5ee2f19c3d4da5d3cd4cb8cbc623
Sebastian Vogeltanz <sebvogeltanz@gmail.com>
2020-02-05 11:36:31 +0100

    made some updates on dictionaries

--- 03_more_datatypes/4_dictionaries/04_18_common.py
+++ 03_more_datatypes/4_dictionaries/04_18_common.py
@@ -10,36 +10,25 @@ result = {"a": 3, "b": 2, "c": 7 , "d": 2}
 '''
 
 
+dict1 = {"a": 1, "b": 2, "c": 3}
+dict2 = {"a": 2, "c": 4 , "d": 2}
+
+# define a function which adds the values together
+def mergeDict(firstDict, secondDict):
+    '''Combining two dictionaries and adding the values of equal keys'''
+
+    # create a new dict to return at the end of the function
+    thirdDict = {**firstDict, **secondDict}
+
+    # check every key, value pair in the new dict
+    for key, value in thirdDict.items():
+        # check if the key of the given dicts is in both dicts
+        if key in firstDict and key in secondDict:
+            # if so, add the values together
+            thirdDict[key] = value + firstDict[key]
+    return thirdDict
 
 
+dict3 = mergeDict(dict1, dict2)
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-dict_1 = {"a": 1, "b": 2, "c": 3}
-dict_2 = {"a": 2, "c": 4 , "d": 2}
-
-# create a function which adds the values for the same key
-def dictAdd(dict1, dict2):
-    dict_result = {**dict_1, **dict_2}
-    for key, value in dict_result.items():
-        if key in dict_1 and key in dict_2:
-            dict_result[key] = value + dict_1[key]
-
-    return dict_result
-
-# merge two dictionaries
-result_dict = dictAdd(dict_1, dict_2)
-print(result_dict)
+print(dict3)
--- 03_more_datatypes/4_dictionaries/04_19_dict_tuples.py
+++ 03_more_datatypes/4_dictionaries/04_19_dict_tuples.py
@@ -7,28 +7,11 @@ result_list = [("item3", 1), ("item1", 5), ("item2", 6)]
 '''
 
 
+input_dict = {"item1": 5, "item2": 6, "item3": 1, "item4": 0, "item5": 100}
 
+# sort the dict with the sorted function
+result_list = sorted(input_dict.items(), key=lambda x: x[1])
+# creates a sorted list by default
 
+print(result_list)
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-input_dict = {"item1": 5, "item2": 6, "item3": 1}
-
-sorted_dict = sorted(input_dict.items() , key = lambda x : x[1])
-# Question for Johnny: What's going on with lambdas??
-
-print(sorted_dict)
