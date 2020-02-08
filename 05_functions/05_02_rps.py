9e7231c6bae201f36536f765cc187367e641d497
Sebastian Vogeltanz <sebvogeltanz@gmail.com>
2020-02-08 16:49:09 +0100

    updated the rps game

--- 05_functions/05_02_rps.py
+++ 05_functions/05_02_rps.py
@@ -1,14 +1,16 @@
 '''
 
-Code a game of rock paper scissors.
+Code a game of rock paper scissors. Update the instructions from the github repo!
+
+- take in a number 1-3 from the user that represents their hand
+- generate a random number 1-3 to use for the computer's hand
+- call the function get_hand to get the string representation of the user's hand
+- call the function get_hand to get the string representation of the computer's hand
+- call the function determine_winner to figure out who won
+- print out the player hand and computer hand
+- print out the winner
 
 '''
-
-# the user needs a hand to choose
-
-# a random hand has to be chosen by the computer/player 2
-
-# check in a dictionary which combination determines who wins
 
 import random
 
@@ -19,99 +21,47 @@ print("Let's play a game of rock, paper, scissors! \n"
       "3. Scissors")
 user_hand = int(input("Please enter the number of your choice: "))
 
-pc_hand = random.randrange(1, 4, 1)
+computer_hand = random.randrange(1, 4, 1)
 
-hand_finder = {1: "Rock", 2: "Paper", 3: "Scissors"}
-
-print("Your hand is: ", hand_finder[user_hand])
-print("The computer's hand is: ", hand_finder[pc_hand])
+# create a function to get the hand of a player's number
+def get_hand(num):
+    if num == 1:
+        return "Rock"
+    elif num == 2:
+        return "Paper"
+    elif num == 3:
+        return "Scissor"
+    else:
+        return "Something went wrong"
 
 # determine who won with a nested if-statement; or use a function to determine winner
-
 def determineWinner(hand1, hand2):
 
-    if user_hand == 1 and pc_hand == 1:
+    if hand1 == 1 and hand2 == 1:
         print("You tie!")
-    elif user_hand == 2 and pc_hand == 1:
+    elif hand1 == 2 and hand2 == 1:
         print("You win!")
-    elif user_hand == 3 and pc_hand == 1:
+    elif hand1 == 3 and hand2 == 1:
         print("You loose!")
-    elif user_hand == 1 and pc_hand == 2:
+    elif hand1 == 1 and hand2 == 2:
         print("You loose!")
-    elif user_hand == 2 and pc_hand == 2:
+    elif hand1 == 2 and hand2 == 2:
         print("You tie!")
-    elif user_hand == 3 and pc_hand == 2:
+    elif hand1 == 3 and hand2 == 2:
         print("You win!")
-    elif user_hand == 1 and pc_hand == 3:
+    elif hand1 == 1 and hand2 == 3:
         print("You win!")
-    elif user_hand == 2 and pc_hand == 3:
+    elif hand1 == 2 and hand2 == 3:
         print("You loose!")
-    elif user_hand == 3 and pc_hand == 3:
+    elif hand1 == 3 and hand2 == 3:
         print("You tie!")
     else:
         print("Something went wrong!")
     return
 
 
-determineWinner(user_hand, pc_hand)
-
-# see below a prior version
-'''
-# function to get hand based on number
-def get_hand(number):
-    number_to_hand = {0: "Scissor", 1: "Rock", 2: "Paper"}
-    new_hand = str(number_to_hand.get(number))
-    return new_hand
-
-# function should take in two hands and return a string "You won!" or "You lost :(" or "You tied!"
-def determine_winner(computer, user):
-    # create new string with both hands to use compare_hands dictionary
-    both_hands = f"{computer} {user}"
-
-    # keep the number of conditionals to a minimum using dictionaries
-    # dictionary for determining the winner/loser
-    compare_hands = {"Scissor Scissor": 2,
-                     "Scissor Rock": 0,
-                     "Scissor Paper": 1,
-                     "Rock Scissor": 1,
-                     "Rock Rock": 2,
-                     "Rock Paper": 0,
-                     "Paper Scissor": 0,
-                     "Paper Rock": 1,
-                     "Paper Paper": 2}
-
-    if compare_hands[both_hands] == 0:
-        result = "User wins!"
-    elif compare_hands[both_hands] == 1:
-        result = "Computer wins!"
-    elif compare_hands[both_hands] == 2:
-        result = "You tied!"
-    else:
-        result = "Something went wrong!"
-    return result
+print("Your hand is: ", get_hand(user_hand))
+print("The computer's hand is: ", get_hand(computer_hand))
 
 
-# take in a number 0-2 from the user that represents their hand
-user_number = int(input("Please enter a number between 0 and 2: "))
-
-# generate a random number 0-2 to use for the computer's hand
-import random
-comp_number = random.randint(0, 3)
-
-# call the function get_hand to get the string representation of the user's hand
-user_hand = get_hand(user_number)
-
-# call the function get_hand to get the string representation of the computer's hand
-comp_hand = get_hand(comp_number)
-
-# call the function determine_winner to figure out the winner
-game_result = determine_winner(comp_hand, user_hand)
-
-# print out the player hand and computer hand
-print(f"The hand of the user is: {user_hand}")
-print(f"The hand of the computer is: {comp_hand}")
-
-# print out the winner
-print(f"And the result of the game is: {game_result}")
-
-'''
+determineWinner(user_hand, computer_hand)
