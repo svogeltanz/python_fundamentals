f9ab881823ac3f2928a2aaf1548fade2240e494b
Sebastian Vogeltanz <sebvogeltanz@gmail.com>
2020-02-19 09:56:39 +0100

    worked on try/except

--- 08_exceptions/08_02_calculator.py
+++ 08_exceptions/08_02_calculator.py
@@ -11,12 +11,18 @@ Test it and make sure it does not crash when you enter incorrect values.
 
 
 try:
+    # take in two number and calculate the quotient
     dividend = int(input("Please enter a dividend: "))
     divisor = int(input("Please enter a divisor: "))
-    result = dividend / divisor
-    print(result)
+    result = dividend/divisor
+    print(f"You want to divide {dividend} by {divisor}.")
+    print(f"The result of your division is: {result}")
 except ZeroDivisionError as zde:
-    print("You cannot divide by 0.")
+    # catch the case if input is 0
+    print("You cannot divide by 0. Please enter a positive number.")
 except ValueError as ve:
-    print("This is not a number.")
-
+    # catch the case if input is a string
+    print("You did not enter a number. Please enter a number.")
+except Exception as err:
+    # catch anything else
+    print("There has been a mistake: ", err)
--- 08_exceptions/08_03_else.py
+++ 08_exceptions/08_03_else.py
@@ -3,17 +3,22 @@ Write a script that demonstrates a try/except/else.
 
 '''
 
-# I used the example from 08_02 and extended it with an else statement
 
 try:
+    # take in two number and calculate the quotient
     dividend = int(input("Please enter a dividend: "))
     divisor = int(input("Please enter a divisor: "))
-    result = dividend / divisor
 except ZeroDivisionError as zde:
-    print("You cannot divide by 0.")
+    # catch the case if input is 0
+    print("You cannot divide by 0. Please enter a positive number.")
 except ValueError as ve:
-    print("This is not a number.")
+    # catch the case if input is a string
+    print("You did not enter a number. Please enter a number.")
+except Exception as err:
+    # catch anything else
+    print("There has been a mistake: ", err)
 else:
-    print("Everything went just fine!")
-    print(f"The result is: {result}")
-
+    result = dividend/divisor
+    print("Your input is pretty good.")
+    print(f"You want to divide {dividend} by {divisor}.")
+    print(f"The result of your division is: {result}")
