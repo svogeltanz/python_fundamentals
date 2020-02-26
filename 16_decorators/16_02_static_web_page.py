9aed50346326561af5607b820c49f4dc357af163
Sebastian Vogeltanz <sebvogeltanz@gmail.com>
2020-02-26 11:03:37 +0100

    worked on decorators

--- 16_decorators/16_02_static_web_page.py
+++ 16_decorators/16_02_static_web_page.py
@@ -8,15 +8,30 @@ The user should be able to decide which tag to use.
 from functools import wraps
 
 
-# write a decorator that takes a function
-def outer(func):
-    def inner(func):
-        # wrap func in html tag
-        return func
-    return inner
+# build a decorator that wraps a given text into an html tag
+def tags(tag_name):
+    def tags_decorator(func):
+        @wraps(func)
+        def func_wrapper(name):
+            return "<{0}>{1}</{0}>".format(tag_name, func(name))
+        return func_wrapper
+    return tags_decorator
 
 
-@outer
-def text(msg):
-    return msg
+tag_list = ['title', 'header', 'h1', 'h2', 'body', 'p']
+
+
+for tag in tag_list:
+    print(f'{tag_list.index(tag)}: {tag}')
+user_tag = input("Please type the tag you want to use: ")
+user_text = input("And now type the text you want to pass in that tag: ")
+
+
+@tags(f"{user_tag}")
+def get_text(text):
+    """returns some text"""
+    return text
+
+
+print(get_text(user_text))
 
